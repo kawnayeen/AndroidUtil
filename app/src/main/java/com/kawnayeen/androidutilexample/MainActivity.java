e315ebb7a0ddd9ab905eb4d780588e9a69a6d827
kawnayeen <kawnayeen.08@gmail.com>
2017-08-24 09:19:07 +0600

    Trying to develop utility module for image picking

--- app/src/main/java/com/kawnayeen/androidutilexample/MainActivity.java
+++ app/src/main/java/com/kawnayeen/androidutilexample/MainActivity.java
@@ -1,13 +1,53 @@
 package com.kawnayeen.androidutilexample;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.kawnayeen.intentutil.ImagePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button clickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clickMe = findViewById(R.id.clickMe);
+        clickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPickImage();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private static final int PICK_IMAGE_ID = 234; // the number doesn't matter
+
+    public void onPickImage() {
+        Intent chooseImageIntent = ImagePicker.getPickImageIntent(this);
+        startActivityForResult(chooseImageIntent, PICK_IMAGE_ID);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PICK_IMAGE_ID:
+                Bitmap bitmap = ImagePicker.getImageFromResult(this, resultCode, data);
+                // TODO use bitmap
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
     }
 }
